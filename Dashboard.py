cbce36958bb93852a12c69391a9313a0f2911eb1
MarkMiddendorf <markmiddendorf49@gmail.com>
2024-10-10 18:08:05 -0400

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -31,7 +31,7 @@ creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 
 # Open Google Sheet by name or URL
-sheet = client.open('https://docs.google.com/spreadsheets/d/1nigC8X7S0L7wBsFOhIz8DpXiyNXXnqHxVEIHdzVWl9k/edit?usp=sharing').worksheet("Data")
+sheet = client.open("DanceToEvolve_Data).worksheet("Data")
 
 # Get data from Google Sheet
 data = sheet.get_all_records()
